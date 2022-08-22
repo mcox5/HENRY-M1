746987f22b5aa123f4209267a8a8d916e6703299
¨´mcox5 <mcox5@uc.cl>
2022-08-22 09:00:34 -0400

    Estructuras de datos I

--- 04-EstructuraDeDatos-I/homework/homework.js
+++ 04-EstructuraDeDatos-I/homework/homework.js
@@ -15,10 +15,33 @@ Como ejercicio adicional y completamente opcional, al terminar de resolver este 
 */
 
 function nFactorial(n) {
+    if (n===1 || n===0) { //recordar QUE SIEMPRE! es buscar el caso BASE en recursión
+        return (1);   
+    } else if (n<0) {
+        return "NO EXISTE FACTORIAL PARA NEGATIVOS";
+    }
+    else {
+        return (n * nFactorial(n-1)); //Aca generalmente va la Fórmula de la recursion!
+    }
+
 }
 
 function nFibonacci(n) {
+  //en la posición 0 de la secuencia vamos a tener cero y en la posición 1 vamos a tener un 1
+  //0=0
+  //1=1
+  //2=1+0=1
+  //3=1+1=2
+  //4=2+1=3
+  //5=3+2=5
+  //en este caso podemos ver que el 0 y el 1 son nuestros casos bases!
+  if (n<0) return "No existe posición negativa"; //ojo que se puede escribir de esta manera también los "if"
+  if (n===0 || n===1) return n;
+  //ya tenemos nuestros casos bases escritos, ahora buscamos la fórmula que se adecuúe a lo que se pida
+  return nFibonacci(n-1)+nFibonacci(n-2)
 }
+
+//console.log(nFibonacci())
 
 /*
 Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde el primer elemento que ingresa es el primero que se quita. Definir los siguientes métodos:
@@ -30,8 +53,24 @@ Pueden utilizar class o función constructora.
 */
 
 function Queue() {
-
+  this.arr=[];
 }
+Queue.prototype.enqueue = function (value) {
+  this.arr.push(value)
+};
+Queue.prototype.dequeue = function () {
+  return this.arr.shift(); // el shift por si solo retorna undefined si es que el array está vacido
+  
+};
+Queue.prototype.size = function () {
+  return this.arr.length;
+};
+const miQueue= new Queue
+console.log(miQueue.size())
+console.log(miQueue.enqueue())
+console.log(miQueue.dequeue())
+
+
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
